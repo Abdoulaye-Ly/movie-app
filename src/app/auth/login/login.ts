fefc1deb92688587d43554d4d788df0f97d2915c
Abdoulaye Ly <abdoulayely536@gmail.com>
2025-07-17 15:17:58 +0100

    add Login Feature

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrls: ['./login.scss'],
 })
 export class Login {
+  username = '';
+  password = '';
 
+  constructor(private router: Router) {}
+
+  login() {
+    if (this.username.trim() && this.password.trim()) {
+      localStorage.setItem('loggedInUser', this.username);
+      this.router.navigate(['/']);
+    } else {
+      alert('Please enter username and password.');
+    }
+  }
 }
